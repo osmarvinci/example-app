114fb29fbe485e70dd5d10042cd9085f64d52e24
osmarvinci <osmarvinci@gmail.com>
2013-09-10 03:12:34 +0000

    insert funcionando

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 if (!global.hasOwnProperty('db')) {
   var Sequelize = require('sequelize')
-    , sequelize = null
+    , sequelize = null;
+     var fs = require('fs');
+     var PGPASS_FILE = '../.pgpass';
 
   if (process.env.HEROKU_POSTGRESQL_ORANGE_URL) {
     // the application is executed on Heroku ... use the postgres database
@@ -15,8 +17,26 @@ if (!global.hasOwnProperty('db')) {
     })
   } else {
     // the application is executed on the local machine ... use mysql
-    sequelize = new Sequelize('example-app-db', 'root', null)
-  }
+   // sequelize = new Sequelize('example-app-db', 'root', null)
+  /* Local database
+           We parse the .pgpass file for the connection string parameters.
+        */
+        var pgtokens = fs.readFileSync(PGPASS_FILE).toString().split(':');
+        var host = pgtokens[0];
+        var port = pgtokens[1];
+        var dbname = pgtokens[2];
+        var user = pgtokens[3];
+        var password = pgtokens[4];
+        var config =  {
+            dialect:  'postgres',
+            protocol: 'postgres',
+            port:     port,
+            host:     host,
+        };
+        sequelize  = new Sequelize(dbname, user, password, config); 
+
+
+ }
 
   global.db = {
     Sequelize: Sequelize,
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,16 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+
+    console.log(req);
+    global.db.User.build({username: 'osmar'})
+    .save()
+    .success(function(){console.log("insert")})
+    .error(function(err){console.log(err)})
+
+    global.db.User.findAll().success(function(user){console.log(user);
+						    res.send(user)
+})
+    .error(function(err){console.log(err); res.send("erro")})
+	  //  res.send("respond with a resource");
 };
